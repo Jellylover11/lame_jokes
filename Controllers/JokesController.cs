4ff54aeec80cc697ad490d872279e4e93a9a8f9b
shohidul <shohidulislam@iut-dhaka.edu>
2023-07-05 16:06:56 +0600

    commit 3

--- Controllers/JokesController.cs
+++ Controllers/JokesController.cs
@@ -27,6 +27,24 @@ namespace lame_jokes.Controllers
                           Problem("Entity set 'ApplicationDbContext.Joke'  is null.");
         }
 
+        //Get: Jokes/ShowSearchForm 
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            return _context.Joke != null ?
+                        View() :
+                        Problem("Entity set 'ApplicationDbContext.Joke'  is null.");
+        }
+
+
+        //Post: Jokes/ShowSearchResults
+        public async Task<IActionResult> ShowSearchResults(String SearchPhrase)
+        {
+            return View("Index",await _context.Joke.Where( j => j.JokeQuestion.Contains(SearchPhrase) ).ToListAsync());
+                       
+                       
+        }
+
+
         // GET: Jokes/Details/5
         public async Task<IActionResult> Details(int? id)
         {
