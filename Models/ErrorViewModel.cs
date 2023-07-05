f4dde1e532d6e0cfe370d68441406eeed79e1044
shohi <shohi@DESKTOP-5T0S0BO>
2023-07-05 12:34:26 +0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace lame_jokes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
